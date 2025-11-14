609c8c602a49f4bf6947cae8d2b278fee58bbb15
tokumikatu <t1okumikatu@gmail.com>
2025-11-14 21:40:50 +0900

    ③UP

---
+++ ③STATIONから各列車ＤＡＴＡ送信/GPT3_1114_train/GPT3_1114_train.ino
@@ -0,0 +1,40 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include "src/constant.h"
+
+
+
+// ===== 受信コールバック =====
+/*
+void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int len) {
+  memcpy(&recvData, incomingData, sizeof(recvData));
+  Serial.print("[Recv Command] ");
+  Serial.println(recvData.command);
+  switch (recvData.command) {
+    case 0: Serial.println("停止"); break;
+    case 1: Serial.println("前進"); break;
+    case 2: Serial.println("後退"); break;
+    default: break;
+  }
+}*/
+
+// ===== 送信完了コールバック =====
+void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status) {
+  //Serial.print("Send Status: ");
+  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
+}
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  ledcAttachChannel(in1, freq, resolution,ch1);
+  ledcAttachChannel(in2, freq, resolution,ch2);
+  APSTA();
+  nowpeer();
+  }
+
+void loop() {
+  //forward(180);
+  readV();
+  delay(2000);
+}
+
